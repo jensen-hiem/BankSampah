d8f7f3f667ccc099703d5d976a1681fe8c21aebe
jensen-hiem <6182201026@student.unpar.ac.id>
2024-11-24 20:29:51 +0700

    Bank Sampah

---
+++ BankSampah/src/main/java/com/example/BankSampah/BankSampahApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankSampah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSampahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSampahApplication.class, args);
+	}
+
+}
