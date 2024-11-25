fe68519dfce758a3fe137fc95e28fc98da535f35
rayner <6182201018@student.unpar.ac.id>
2024-11-25 22:57:16 +0700

    Repository

---
+++ BankSampah/src/main/java/com/example/BankSampah/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.example.BankSampah;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+    
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource datasource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+}
